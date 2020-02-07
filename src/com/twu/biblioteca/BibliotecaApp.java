2370bd361d83e8df3789bbb71033a9bea1dde468
YunQi1994 <317458884@qq.com>
2020-02-07 21:01:14 +0800

    add Author and Year to books; better printing

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,17 +1,28 @@
 package com.twu.biblioteca;
 
+import org.junit.Test;
+
 import java.util.Arrays;
 
 public class BibliotecaApp {
 
     public static void main(String[] args) {
         BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        String[][] str = bibliotecaApp.getList();
         System.out.println("Welcome to Biblioteca. Your one-step-shop for great book titles in Bangalore!");
-        System.out.println("Books We Have:\n"+Arrays.toString(bibliotecaApp.getList()));
+        System.out.println("Books We Have:");
+        for(int i = 0; i<str.length; i++ ){
+            String[] oneBook = str[i];
+            System.out.println(oneBook[0]);
+            for(int j = 1; j<oneBook.length; j++){
+                System.out.println(oneBook[j]);
+            }
+        }
+
 
     }
 
-    public String[] getList() {
+    public String[][] getList() {
         BookLists bookList = new BookLists();
         return bookList.getBookList();
     }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -6,17 +6,17 @@ import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
 
+    //@Test
+    //public void test() {
+    //    assertEquals(1, 1);
+    //}
     @Test
-    public void test() {
-        assertEquals(1, 1);
-    }
-
     public void shouldReturnAListOfBooksAfterWelcome(){
 
         BookLists bookList = new BookLists();
         BibliotecaApp bibliotecaApp = new BibliotecaApp();
 
-        String[] expectBookList = bookList.getBookList();
+        String[][] expectBookList = bookList.getBookList();
         assertEquals(expectBookList, bibliotecaApp.getList());
     }
 }
