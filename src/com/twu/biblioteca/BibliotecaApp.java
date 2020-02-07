542248db91042a6a857daa4e19b85ed44c5b56ac
YunQi1994 <317458884@qq.com>
2020-02-07 20:24:55 +0800

    print booklist after welcome, but print looks ungly

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,9 +1,13 @@
 package com.twu.biblioteca;
 
+import java.util.Arrays;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
         System.out.println("Welcome to Biblioteca. Your one-step-shop for great book titles in Bangalore!");
+        System.out.println("Books We Have:\n"+Arrays.toString(bibliotecaApp.getList()));
 
     }
 
