d8c9106500d44092d8bcf861dc8500d026b0e4d8
YunQi1994 <317458884@qq.com>
2020-02-09 20:50:22 +0800

    test for checkout A book passed

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -8,6 +8,7 @@ public class BibliotecaApp {
 
     public static void main(String[] args) {
 
+
         System.out.println("Welcome to Biblioteca. Your one-step-shop for great book titles in Bangalore!");
         openMenu();
 
--- src/com/twu/biblioteca/BookLists.java
+++ src/com/twu/biblioteca/BookLists.java
@@ -1,13 +1,30 @@
 package com.twu.biblioteca;
 
+import java.util.Arrays;
+
 public class BookLists {
     private String[][] bookList = {
-            {"Title: book1", "Author: Hugh", "YearOfPub: 2001"},
-            {"Title: book2", "Author: Jack", "YearOfPub: 2002"},
-            {"Title: book3", "Author: Man", "YearOfPub: 2003"}};
+            {"book1", "Author: Hugh", "YearOfPub: 2001"},
+            {"book2", "Author: Jack", "YearOfPub: 2002"},
+            {"book3", "Author: Man", "YearOfPub: 2003"}};
 
     public String[][] getBookList() {
         return bookList;
     }
+
+    public void checkOutABook(String bookTitle) {
+        int len = bookList.length;
+        String[][] newBookList = new String[len-1][3];
+        int i = 0;
+        for (String[] str : bookList){
+            if (!bookTitle.equals(str[0])){
+                //System.out.println("111111");
+                newBookList[i] = str;
+                i ++;
+            }
+        }
+        bookList = newBookList;
+    }
+
 
 }
