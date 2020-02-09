3e1d4010533d4accffe3d8352d6d3216cfe8a316
YunQi1994 <317458884@qq.com>
2020-02-09 19:34:23 +0800

    test for checkout A book

--- src/com/twu/biblioteca/BookLists.java
+++ src/com/twu/biblioteca/BookLists.java
@@ -1,11 +1,12 @@
 package com.twu.biblioteca;
 
 public class BookLists {
-    private String [][] bookList = {
-            {"Title: book1","Author: Hugh","YearOfPub: 2001"},
-            {"Title: book2","Author: Jack","YearOfPub: 2002"},
-            {"Title: book3","Author: Man","YearOfPub: 2003"}};
-    public String[][] getBookList(){
+    private String[][] bookList = {
+            {"Title: book1", "Author: Hugh", "YearOfPub: 2001"},
+            {"Title: book2", "Author: Jack", "YearOfPub: 2002"},
+            {"Title: book3", "Author: Man", "YearOfPub: 2003"}};
+
+    public String[][] getBookList() {
         return bookList;
     }
-
+}
