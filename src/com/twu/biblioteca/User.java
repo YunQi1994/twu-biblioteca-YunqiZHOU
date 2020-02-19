c0f63a6c5b80284e9cca0d63bade1c508fa1d50a
YunQi1994 <317458884@qq.com>
2020-02-19 11:39:34 +0800

    clean no use methods in class User

--- src/com/twu/biblioteca/User.java
+++ src/com/twu/biblioteca/User.java
@@ -12,17 +12,7 @@ public class User {
         return users;
     }
 
-    public boolean isUserExist (String userName) {
-        boolean isExist = false;
-        for (String [] user : users){
-            if (userName.equals(user[0])){
-                // user found
-                isExist = true;
-                break;
-            }
-        }
-        return isExist;
-    }
+
 
     public String[] getUser(String userName) {
         String [] result = new String[users[0].length];
@@ -36,40 +26,9 @@ public class User {
         return result;
     }
 
-    public boolean isPasswordCorrect(String password){
-        boolean iscorrect = false;
-        for (String [] user : users){
-            if (password.equals(user[1])){
-                // password is correct
-                iscorrect = true;
-                System.out.println("pass");
-                break;
-            }
-            else{
-                System.out.println("Sorry, the password is not correct\n");
-                break;
-            }
-        }
-        return iscorrect;
-    }
 
-    public boolean isUserNameAndPasswordCorrect(String userName, String password) {
-        boolean isVerified = false;
-        for (String [] user : users){
-            if (isUserExist(userName)){
-                // user found
-                if (password.equals(user[1])){
-                    // password is correct
-                    isVerified = true;
-                    break;
-                }
-            }
-            else {
-                System.out.println("Sorry user not found\n");
-            }
-        }
-        return isVerified;
-    }
+
+
 
     public void userLogin (String userName){
         getUser(userName)[2] = "onLine";
